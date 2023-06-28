a3f25e20dd7262877c0901e34c603738993c15ac
ulumbagas <ulumbagas@gmail.com>
2023-06-28 23:41:58 +0700

    update fix GPA data

--- code_cleaning_data.sql
+++ code_cleaning_data.sql
@@ -1,18 +1,25 @@
-select * from food_1;
+select * from food;
 
 #update text data to "nan"
-UPDATE food_1 
+UPDATE food 
 SET 
     GPA = 'nan'
 WHERE
     GPA IN  ('Unknown','Personal');
 
-/*UPDATE food_1 
+
+
+#update text data to delete text
+UPDATE food 
+SET 
+    GPA = '3.79'
+WHERE
+    GPA = '3.79 bitch';
+
+/*UPDATE food 
 SET 
     GPA = 'nan'
 WHERE
     GPA like '%Unknown&' or GPA like '%Personal%';
-    
  */
  
-select distinct(GPA) from food_1;
