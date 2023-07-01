0d8dcd9e0a63c2595cdcd833e2f3bf36cbbaa39e
ulumbagas <ulumbagas@gmail.com>
2023-07-01 21:04:54 +0700

    delete unnecessery string

--- code_cleaning_data.sql
+++ code_cleaning_data.sql
@@ -1,6 +1,8 @@
-select * from food;
+select GPA from food
+where GPA = 'Personal ';
 
-select right(comfort_food,5) from food;
+select comfort_food, right(comfort_food,1) from food
+where right(comfort_food,1) = ',';
 
 
 #update text data to "nan"
@@ -40,5 +42,21 @@ UPDATE food SET
 comfort_food = REPLACE (comfort_food,"-"," ")
 WHERE comfort_food LIKE '%-%';
 
+UPDATE food SET 
+comfort_food = REPLACE (comfort_food,"/",",")
+WHERE comfort_food LIKE '%/%';
+
+
 update food set 
 comfort_food = trim(comfort_food);
+
+select comfort_food as 'Before',comfort_food_copy1 as 'After' from food
+WHERE right(comfort_food_copy1,1) in (',','.');
+
+update food set 
+comfort_food_copy1 = comfort_food;
+
+
+UPDATE food
+SET comfort_food = CONCAT(SUBSTRING(comfort_food, 1, LENGTH(comfort_food) - 1), '')
+WHERE right(comfort_food,1) in (',','.');
